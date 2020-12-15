e0c29b06cd6b7e1f6b9b397493a602d9077e14ef
Jeffrey Lo-A-Foe <17035570@student.hhs.nl>
2020-12-15 13:55:09 +0100

    added project page and details

--- DonationPlatform/DonationPlatform/Controllers/ProjectController.cs
+++ DonationPlatform/DonationPlatform/Controllers/ProjectController.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using DonationPlatform.Models;
 
 namespace DonationPlatform.Controllers
 {
@@ -11,6 +13,19 @@ namespace DonationPlatform.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            ProjectModel project = new ProjectModel("Project" + id);
+
+            if(id == null)
+            {
+                project = new ProjectModel("Unknown");
+            }
+
+            return View(project);
+            
         }
     }
 }
