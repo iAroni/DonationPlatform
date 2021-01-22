cd55edb8c44b701461fce523317ae345aa81f94d
Aron van Leeuwen <35957864+iAroni@users.noreply.github.com>
2021-01-22 22:28:58 +0100

    add owner role
    and did some styling changes

--- TruffleContract/contracts/Dochaintion.sol
+++ TruffleContract/contracts/Dochaintion.sol
@@ -1,26 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Dochaintion {
+contract Dochaintion is AccessControl {
     //Structs
     struct Project {
-      uint projectId;
-      address projectAddress;
-      string projectName;
-      string name;
-      string description;
-      string photoCid;
-      uint totalDonation;
-      bool isActive;
-    } 
+        uint256 projectId;
+        address projectAddress;
+        string projectName;
+        string name;
+        string description;
+        string photoCid;
+        uint256 totalDonation;
+        bool isActive;
+    }
 
     struct Donation {
-      address donator;
-      uint amount;
-      address projectAddress;
-      string projectName;
-      uint date;
+        address donator;
+        uint256 amount;
+        address projectAddress;
+        string projectName;
+        uint256 date;
     }
 
     //Arrays
@@ -29,79 +30,120 @@ contract Dochaintion {
 
     //Mappings
     mapping(address => Project[]) founderToProject;
-    mapping(uint => Project) idToProject;
+    mapping(uint256 => Project) idToProject;
 
     //Events
     event projectMade(address founder, Project madeProject);
     event donationMade(Donation donation);
 
+    //Rolls
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+
+    //Constructor
+    constructor(address owner1, address owner2) public {
+        _setupRole(OWNER_ROLE, owner1);
+        _setupRole(OWNER_ROLE, owner2);
+    }
+
     //Project creation
-    function makeProject(address _projectAddress, string memory _projectName ,string memory _name, string memory _description, string memory _photoCid) public {
+    function makeProject(
+        address _projectAddress,
+        string memory _projectName,
+        string memory _name,
+        string memory _description,
+        string memory _photoCid
+    ) public {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller is not the owner");
+        //Get latest number for project id.
+        uint256 currentLatestProjectNumber = projectsList.length;
 
-      //Get latest number for project id.
-      uint currentLatestProjectNumber = projectsList.length;
+        //Make project with given information
+        Project memory newProject =
+            Project(
+                currentLatestProjectNumber,
+                _projectAddress,
+                _projectName,
+                _name,
+                _description,
+                _photoCid,
+                0,
+                true
+            );
 
-      //Make project with given information
-      Project memory newProject = Project(currentLatestProjectNumber,_projectAddress, _projectName, _name, _description, _photoCid,0 , true);
+        //Save the project for lookup
+        projectsList.push(newProject);
+        founderToProject[msg.sender].push(newProject);
+        idToProject[currentLatestProjectNumber] = newProject;
 
-      //Save the project for lookup
-      projectsList.push(newProject);
-      founderToProject[msg.sender].push(newProject);
-      idToProject[currentLatestProjectNumber] = newProject;
-
-      //Event
-      emit projectMade(msg.sender, newProject);
+        //Event
+        emit projectMade(msg.sender, newProject);
     }
 
-    function fundProject(uint chosenProjectId) public payable {
-      //Check if ETH amount is send and a project is found
-      require(msg.value > 0, "No ETH sended for fund");
-      require(chosenProjectId >= 0, "No project found");
+    function fundProject(uint256 chosenProjectId) public payable {
+        //Check if ETH amount is send and a project is found
+        require(msg.value > 0, "No ETH sended for fund");
+        require(chosenProjectId >= 0, "No project found");
 
-      //check if call succeeded go further
-      (bool success, ) = idToProject[chosenProjectId].projectAddress.call{value : msg.value}('');
-      require(success, "Transfer was not succesfull");
-      
-      idToProject[chosenProjectId].totalDonation += msg.value;
-      projectsList[chosenProjectId].totalDonation += msg.value;
+        //check if call succeeded go further
+        (bool success, ) =
+            idToProject[chosenProjectId].projectAddress.call{value: msg.value}(
+                ""
+            );
+        require(success, "Transfer was not succesfull");
 
-      //Save donation in chain
-      Donation memory madeDonation = Donation(
-        msg.sender,
-        msg.value,
-        idToProject[chosenProjectId].projectAddress,
-        idToProject[chosenProjectId].projectName,
-        block.timestamp
-      );
-      donationsList.push(madeDonation);
+        idToProject[chosenProjectId].totalDonation += msg.value;
+        projectsList[chosenProjectId].totalDonation += msg.value;
 
-      //Event it
-      emit donationMade(madeDonation);
+        //Save donation in chain
+        Donation memory madeDonation =
+            Donation(
+                msg.sender,
+                msg.value,
+                idToProject[chosenProjectId].projectAddress,
+                idToProject[chosenProjectId].projectName,
+                block.timestamp
+            );
+        donationsList.push(madeDonation);
+
+        //Event it
+        emit donationMade(madeDonation);
     }
 
-    function getAllProjects() public view returns(Project[] memory) {
-      return projectsList;
+    function getAllProjects() public view returns (Project[] memory) {
+        return projectsList;
     }
 
-    function getProject(uint number) public view returns(Project memory){
-      require(projectsList[number].isActive," This project is inactive");
+    function getProject(uint256 number) public view returns (Project memory) {
+        require(projectsList[number].isActive, " This project is inactive");
 
-      return projectsList[number];
+        return projectsList[number];
     }
 
-    function deactiveProject(uint number) public {
-      address caller = msg.sender;
-      projectsList[number].isActive = false;
-      for(uint i = 0; i < founderToProject[msg.sender].length; i++){
-          if(founderToProject[caller][i].projectId == number){
-            founderToProject[caller][i].isActive = false;
-          }
-      }
-      idToProject[number].isActive = false;
+    function getAllDonations() public view returns (Donation[] memory) {
+        return donationsList;
     }
 
-    function getAllDonations() public view returns(Donation[] memory) {
-      return donationsList;
+    function deactiveProject(uint256 number) public {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller is not the owner");
+        address caller = msg.sender;
+        projectsList[number].isActive = false;
+        for (uint256 i = 0; i < founderToProject[msg.sender].length; i++) {
+            if (founderToProject[caller][i].projectId == number) {
+                founderToProject[caller][i].isActive = false;
+            }
+        }
+        idToProject[number].isActive = false;
     }
 
+    function activateProject(uint256 number) public {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller is not the owner");
+        address caller = msg.sender;
+        projectsList[number].isActive = true;
+        for (uint256 i = 0; i < founderToProject[msg.sender].length; i++) {
+            if (founderToProject[caller][i].projectId == number) {
+                founderToProject[caller][i].isActive = true;
+            }
+        }
+        idToProject[number].isActive = true;
+    }
 }
